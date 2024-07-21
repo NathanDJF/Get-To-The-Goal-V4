a678eacf01998c821ecc62d35b860b7a852cc37c
NathanDJF <nathandjfcodes@gmail.com>
2024-07-21 15:41:15 +0700

    frst comit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+import os
+
+# me when the initialize
+pygame.init()
+pygame.mixer.init()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    pygame.display.update()
